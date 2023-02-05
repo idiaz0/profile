c3e502b8a5a4f5d0b247b6a73248e6f24424fb0c
IVAN DIAZ <116578623+idiaz0@users.noreply.github.com>
2023-02-05 02:57:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -35,5 +35,24 @@ $(document).ready(function(){
       opacity: '0.7',
 	  top: '150px',
     }, 1000);
+	
+	
+	
+	function isScrolledIntoView(elem) {
+    var 
+	docViewTop = $(window).scrollTop(), 
+	docViewBottom = docViewTop + $(window).height(), 
+	elemTop = $(elem).offset().top;
+	
+    return ((elemTop <= docViewBottom));
+}
+
+	$(window).scroll(function(){
+		isScrolledIntoView("[class*='card1']") ? $("[class*='card1']").addClass('animated fadeInDown') : $("[class*='card1']").removeClass('animated fadeInDown')
+		isScrolledIntoView("[class*='card2']") ? $("[class*='card2']").addClass('animated fadeInUp') : $("[class*='card2']").removeClass('animated fadeInUp')
+		isScrolledIntoView("[class*='card3']") ? $("[class*='card3']").addClass('animated fadeInLeft') : $("[class*='card3']").removeClass('animated fadeInLeft')
+		isScrolledIntoView("[class*='card4']") ? $("[class*='card4']").addClass('animated flipInY') : $("[class*='card4']").removeClass('animated flipInY')
+		isScrolledIntoView("[class*='card5']") ? $("[class*='card5']").addClass('animated flipInY') : $("[class*='card5']").removeClass('animated flipInY')
+	});
 
 });	
